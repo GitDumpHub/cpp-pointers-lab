b4e19de32311fdb5c9dbb0ec4f5d8c565b677dee
Альченбаев Ильяс Ильнурович <alichenbaevii@it3.local>
2025-12-17 15:12:38 +0700

    Добавлена программа по указателям и ссылкам

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int x = 100;
+	int* ptr = &x;
+	int& ref = x;
+	cout << "Значение х - " << x << endl;
+	cout << "Значение *ptr - " << &x << endl;
+	cout << "Значение ref - " << ref << endl;
+}
